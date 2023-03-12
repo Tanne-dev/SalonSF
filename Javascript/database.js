5cbc70bf46cde4722973f15bc112fde0733fc089
Tanne <quachtienthanh92@gmail.com>
2023-03-12 18:35:46 +0100

    Update new function sign up,sign in and add info to hero interface

--- Javascript/database.js
+++ Javascript/database.js
@@ -21,3 +21,28 @@ for (let i = 0; i < navItems.length; i++) {
         subElement.style.display = "none";
     });
 }
+// Turn on  clicks on sign in
+let signinBTN = document.querySelector(".btn-login");
+signinBTN.addEventListener("click", function () {
+    let loginElement = document.querySelector("#login-page");
+    loginElement.style.left = "50%";
+    let btnExitlogin = document.querySelector("#login-page .btn-exit");
+    btnExitlogin.addEventListener("click", function () {
+        let loginElement = document.querySelector("#login-page");
+        loginElement.style.left = "-100%";
+    });
+});
+
+// Turn on and off when clicks on sign up
+let regBTN = document.querySelector(".btn-register");
+regBTN.addEventListener("click", function () {
+    let regElement = document.querySelector("#sign-up-Page");
+    regElement.style.right = "20%";
+    regElement.style.display = "block";
+});
+let btnExitreg = document.querySelector("#sign-up-Page .btn-exit");
+btnExitreg.addEventListener("click", function () {
+    let regElement = document.querySelector("#sign-up-Page");
+    regElement.style.right = "-100%";
+    regElement.style.display = "none";
+});
