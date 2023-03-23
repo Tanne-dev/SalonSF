4c42e8948d868fb7ae3490c79b9d0dd98d3708ee
Tanne <quachtienthanh92@gmail.com>
2023-03-23 12:30:22 +0100

    Fixed UI theme and layout for header,hero,service layout

--- Javascript/database.js
+++ Javascript/database.js
@@ -4,10 +4,10 @@ navItems.forEach(function (navItem) {
     let navTitle = navItem.querySelector(".nav-title");
 
     navItem.addEventListener("mouseover", function () {
-        navTitle.style.color = "#fff";
+        navTitle.style.color = "#777";
     });
     navItem.addEventListener("mouseleave", function () {
-        navTitle.style.color = "#eee5d6";
+        navTitle.style.color = "#000000";
     });
 });
 // Show the sub-menu and change the color when mouseover
@@ -21,20 +21,9 @@ for (let i = 0; i < navItems.length; i++) {
         subElement.style.display = "none";
     });
 }
-// Turn on  clicks on sign in
-let signinBTN = document.querySelector(".btn-login");
-signinBTN.addEventListener("click", function () {
-    let loginElement = document.querySelector("#login-page");
-    loginElement.style.left = "50%";
-    let btnExitlogin = document.querySelector("#login-page .btn-exit");
-    btnExitlogin.addEventListener("click", function () {
-        let loginElement = document.querySelector("#login-page");
-        loginElement.style.left = "-100%";
-    });
-});
 
 // Turn on and off when clicks on sign up
-let regBTN = document.querySelector(".btn-register");
+let regBTN = document.querySelector(".header_cta");
 regBTN.addEventListener("click", function () {
     let regElement = document.querySelector("#sign-up-Page");
     regElement.style.display = "block ";
@@ -108,7 +97,12 @@ itemsServiceElement.forEach(function (item) {
         const contentId = item.getAttribute("data-content");
         let serviceBlockElement = document.getElementById("service_block");
         const content = document.getElementById(contentId);
+        const containerElement =
+            serviceBlockElement.querySelector(".service_container");
+        containerElement.style.opacity = "1";
         serviceBlockElement.style.paddingBottom = "200px";
         itemsContainer.innerHTML = content.innerHTML;
     });
 });
+// show image  smooth
+document.querySelector(".hero-img").classList.add("show");
